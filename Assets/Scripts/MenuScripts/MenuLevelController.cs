11f105c00ca2a912a4b0253bab724814b29393c2
Prayten <denvyth@mail.ru>
2018-11-13 11:51:23 +0900

    Добавил выделение и таймер
    Сделал более видимым UI элементы и добавил таймер

---
+++ Assets/Scripts/MenuScripts/MenuLevelController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuLevelController : MonoBehaviour
+{
+
+
+
+    public Button unpauseMenu;
+    public Button quitMenu;
+    public Button pauseMenu;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button unpause = unpauseMenu.GetComponent<Button>();
+        unpause.onClick.AddListener(TaskOnClickUnpause);
+        Button pause = pauseMenu.GetComponent<Button>();
+        pause.onClick.AddListener(TaskOnClickPause);
+        Button quit = quitMenu.GetComponent<Button>();
+        quit.onClick.AddListener(TaskOnClickQuit);
+
+
+    }
+
+    public void TaskOnClickUnpause()
+    {
+        Debug.Log("You have clicked the button");
+        OnClickUnpause();
+    }
+
+    public void TaskOnClickPause()
+    {
+        Debug.Log("You have clicked the button");
+        OnClickPause();
+    }
+
+    public void TaskOnClickQuit()
+    {
+        Debug.Log("You have clicked the button");
+        OnClickQuit();
+    }
+
+    void OnClickUnpause()
+    {
+        print("Continue...");
+        Time.timeScale = 1;
+    }
+
+    void OnClickPause()
+    {
+        print("Pause...");
+        Time.timeScale = 0;
+    }
+
+    void OnClickQuit()
+    {
+        print("Quiting...");
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+
+}
---
+++ Assets/Scripts/MenuScripts/TimerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public Text timerLabel;
+    public float lvlTime;
+
+    void Update()
+    {
+        lvlTime -= Time.deltaTime;
+
+        var minutes = lvlTime / 60; //Divide the guiTime by sixty to get the minutes.
+        var seconds = lvlTime % 60;//Use the euclidean division for the seconds.
+        var fraction = (lvlTime * 100) % 100;
+
+        //update the label value
+        timerLabel.text = string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
+    }
+}
