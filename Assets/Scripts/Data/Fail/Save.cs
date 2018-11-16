4e3b6dbd7886daf50d19d4060fa241fb670ba82b
Prayten <denvyth@mail.ru>
2018-11-16 19:54:23 +0900

    Json Сохранения

---
+++ Assets/Scripts/Data/Fail/Save.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class Save{
+
+    private Load load;
+
+    private int[] progress = new int[2];
+
+    public void saveLvls()
+    {
+        progress = load.getProgressSave();
+        for (int i = 0; i < progress.Length; i++)
+        {
+            PlayerPrefs.SetInt("lvl" + (i + 1), progress[i]);
+        }
+    }
+        
+    public void setProgressComplete(int lvl)
+    {
+        progress[lvl - 1] = 1;
+    }
+
+}
---
+++ Assets/Scripts/Data/LoadLvlData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+public class LoadLvlData : MonoBehaviour {
+
+    public Text text;
+
+    [SerializeField]
+    private string lvlPath;
+    
+    private LvlCollectior lvlCollectior;
+	
+    private void LoadLvls()
+    {
+        using (StreamReader stream = new StreamReader(lvlPath))
+        {
+            string json = stream.ReadToEnd();
+            print(json);
+            lvlCollectior = JsonUtility.FromJson<LvlCollectior>(json);
+        }
+        print(lvlCollectior.lvlDatas[0].lvlName);
+        Debug.Log("Levels loaded: " + lvlCollectior.lvlDatas.Length);
+        text.text = lvlCollectior.ToString();
+    }
+
+    void Start()
+    {
+        LoadLvls();
+    }
+}
